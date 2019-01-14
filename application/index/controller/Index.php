be80276a26be4e3283da1dbab417b9d124eafa02
小人物 <491376424@qq.com>
2019-01-14 19:07:40 +0800

    9、事务

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,10 +2,44 @@
 namespace app\index\controller;
 
 use think\Collection;
+use think\Db;
+use think\Exception;
 use think\Request;
 use think\url;
 class Index extends  Collection
 {
+    /**
+     * 事务
+     */
+    public function sw(){
+        echo "sw";
+      // dump(Db::table('tp_data')->delete(2));
+        /**
+         * 自动完成
+         */
+//       Db::transaction(function (){
+//           Db::table('tp_data')->delete(2);
+//           Db::table('tp_data')->insert(['name'=>'thinkphp','status'=>1]);
+//       });
+        /**
+         * 手动完成
+         */
+//        Db::startTrans();
+//        try{
+//            Db::table('tp_data')->delete(2);
+//            Db::table('tp_data')->insert(["id"=>8,'name'=>'thinkphp','status'=>1]);
+//            //事务提交
+//            Db::commit();
+//        }catch(Exception $e){
+//            //事务回滚
+//            echo '事务回滚';
+//            Db::rollback();
+//        }
+    }
+    public function sql(){
+        $result = Db::execute('insert into tp_data(name,status) values ("1111",1)');
+        dump($result);
+    }
     public function index()
     {
         return "今天是：".date("Y-m-d H:i:s");
